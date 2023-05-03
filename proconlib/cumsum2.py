99d62b363231746a411380b8dd8cbd5eb6a4b8b0
Akira Hayakawa <ruby.wktk@gmail.com>
2023-05-03 10:57:24 +0900

    upd

--- proconlib/cumsum2.py
+++ proconlib/cumsum2.py
@@ -1,14 +1,16 @@
+from typing import *
+
 class Cumsum2:
-    def __init__(self, h, w, default=0):
+    def __init__(self, h: int, w: int, default=0):
         self.dp = [[default for _ in range(w+1)] for j in range(h+1)]
 
-    def set(self, i, j, x):
+    def set(self, i: int, j: int, x):
         self.dp[i+1][j+1] = x
 
     def build(self):
         double_sweep(self.dp)
 
-    def query(self, p, q):
+    def query(self, p: Tuple[int,int], q: Tuple[int,int]):
         sum1 = self.dp[p[0]][p[1]]
         sum4 = self.dp[q[0]][q[1]]
         sum2 = self.dp[p[0]][q[1]]
