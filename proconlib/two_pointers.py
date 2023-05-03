03ef4b06b2ded3bb9affe993ce94ae3e97f94717
Akira Hayakawa <ruby.wktk@gmail.com>
2023-05-03 16:53:47 +0900

    upd

--- proconlib/two_pointers.py
+++ proconlib/two_pointers.py
@@ -1,24 +1,24 @@
 from typing import *
 
 
-def two_pointers(start: int, end: int, f: Callable[[int,int],bool], g: Callable[[int,int], NoReturn]):
+def two_pointers(start: int, end: int, f, g):
     l = start
     r = start
     while not (l == end and r == end):
         cur = f(l, r)
         if r < end:
-            if cur:
-                nxt = f(l, r+1)
-                if nxt:
-                    r += 1
-                else:
-                    if l < r:
-                        g(l, r)
-                        l += 1
-                    else:
-                        r += 1
+            nxt = f(l, r+1)
+            if nxt:
+                r += 1
             else:
-                l += 1
+                if l < r:
+                    if cur:
+                        g(l, r)
+                    l += 1
+                else:
+                    if cur:
+                        g(l,r)
+                    r += 1
         else:
             if cur:
                 g(l, r)
