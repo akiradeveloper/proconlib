51c4d338fafb0905d178b80ef52215b923e6afb2
Akira Hayakawa <ruby.wktk@gmail.com>
2023-05-05 20:46:56 +0900

    upd

--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -1,4 +1,5 @@
 from proconlib.binary_search import binary_search
+import pytest
 
 
 def test_binary_search():
@@ -8,3 +9,16 @@ def test_binary_search():
     assert binary_search(0, 9, lambda i: A[i] > 10) == 9
     assert binary_search(0, 9, lambda i: A[i] > 0) == 0
     assert binary_search(0, 9, lambda i: A[i] > 1) == 1
+
+
+@pytest.mark.randomize(l=pytest.list_of(int, items=10000), x=int)
+def test_binary_search_quichcheck(l, x):
+    l.sort()
+    n = len(l)
+    l1 = binary_search(0, n, lambda i: l[i] >= x)
+    r1 = binary_search(0, n, lambda i: l[i] > x)
+    import bisect
+    l2 = bisect.bisect_left(l, x)
+    r2 = bisect.bisect_right(l, x)
+    assert l1 == l2
+    assert r1 == r2
