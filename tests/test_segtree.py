5c7f41cca8ffab756187552651b15ae62d913396
Akira Hayakawa <ruby.wktk@gmail.com>
2023-05-13 10:21:34 +0900

    upd

--- tests/test_segtree.py
+++ tests/test_segtree.py
@@ -1,4 +1,4 @@
-from proconlib.segtree import *
+from proconlib.segtree import SegTree, LazySegTree
 
 import pytest
 
@@ -24,3 +24,30 @@ def test_segtree(T):
             for r in range(n):
                 if l < r:
                     assert S.fold(l,r) == max(L[l:r])
+
+@pytest.mark.randomize(T=pytest.list_of((int, int, int), items=30), min_num=0, max_num=50)
+def test_lazysegtree(T):
+    def F(*xs):
+        out = 0
+        for x in xs:
+            out = max(out, x)
+        return out
+    
+    def G(x, a):
+        return max(x,a)
+    
+    n = 50
+    L = [0 for _ in range(n)]
+    S = LazySegTree(n, F, F, G)
+    for l,r,x in T:
+        # set
+        l,r = min(l,r), max(l,r)
+        for i in range(l,r):
+            L[i] = x
+            S.set_val(i, x)
+
+        # compare
+        for j in range(n):
+            for k in range(n):
+                if j < k:
+                    assert S.fold(j,k) == max(L[j:k])
